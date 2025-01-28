8e6f4f485197ab2bcb43b3db92b3d3f378ee60c3
Mürsel SEN <murselsen_35@hotmail.com>
2025-01-29 01:01:24 +0300

    Enhance task management by updating rendering logic and resetting task counts

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,19 @@ const application = {
 	elements: {
 		taskList: document.querySelector('#task-list'),
 		taskForm: document.querySelector('#todo-form'),
+		taskCreatedCount: document.querySelector('#task-created-count'),
+		taskCompletedCount: document.querySelector('#task-completed-count'),
 	},
 	data: [],
-	render() {
-		this.elements.taskList.innerHTML = ' ';
-		console.log('Application Rendered');
-		this.data.forEach(task => {
-			const _taskItem = document.createElement('li');
-			_taskItem.id = `task-${task.id}`;
-			_taskItem.className = task.done ? 'task-item done' : 'task-item';
-			_taskItem.innerHTML = `
-                <span class="task-checkbox">${task.done ? '✓' : ''}</span>
-                <p class="task-text">${task.text}</p>
-                <button type="button" class="task-delete-btn">
-                    <i class="fa fa-trash"></i>
-                </button>
-            `;
-			this.elements.taskList.appendChild(_taskItem);
-		});
-	},
+
 	init() {
 		const taskList = localStorage.getItem('tasks');
-		if (taskList !== '1') {
+		if (taskList !== null) {
 			this.data = JSON.parse(taskList);
 		} else {
 			this.data = [];
 		}
 		this.render();
-		console.log(this);
 	},
 	// Random number generator simple method
 	random(min, max) {
@@ -47,10 +32,66 @@ const application = {
 		this.render();
 	},
 	removeTask(taskId) {
+		console.log('Remove Task');
+		taskId = Number(taskId);
+		console.log(taskId);
 		this.data = this.data.filter(task => task.id !== taskId);
+		console.log(this.data);
 		localStorage.setItem('tasks', JSON.stringify(this.data));
+		this.render();
 	},
-	doneTask() {},
+	doneTask(taskId) {
+		taskId = Number(taskId);
+		this.data = this.data.map(task =>
+			task.id === taskId ? { ...task, done: !task.done } : task,
+		);
+		localStorage.setItem('tasks', JSON.stringify(this.data));
+		console.log(this.data);
+		this.render();
+	},
+	render() {
+		if (this.data.length !== 0) {
+			this.elements.taskCreatedCount.innerText = this.data.length.toString();
+			this.elements.taskCompletedCount.innerText =
+				this.data.filter(task => task.done).length + ' de ' + this.data.length;
+
+			this.elements.taskList.innerHTML = ' ';
+			// console.log('Application Rendered');
+
+			this.data.forEach(task => {
+				const _taskItem = document.createElement('li');
+				_taskItem.id = `${task.id}`;
+				_taskItem.className = task.done ? 'task-item done' : 'task-item';
+				_taskItem.innerHTML = `
+                <span class="task-checkbox">${task.done ? '✓' : ''}</span>
+                <p class="task-text">${task.text}</p>
+                <button type="button" class="task-delete-btn">
+                    <i class="fa fa-trash"></i>
+                </button>
+            `;
+				this.elements.taskList.appendChild(_taskItem);
+			});
+
+			document.querySelectorAll('.task-checkbox').forEach(taskCheckbox => {
+				taskCheckbox.addEventListener('click', event => {
+					const taskId = event.target.parentNode.id;
+					application.doneTask(taskId);
+				});
+			});
+
+			document.querySelectorAll('.task-delete-btn').forEach(taskDeleteBtn => {
+				taskDeleteBtn.addEventListener('click', event => {
+					const taskItemDeleteId = event.target.parentNode.parentNode.id;
+
+					application.removeTask(taskItemDeleteId);
+				});
+			});
+		} else {
+			this.elements.taskCreatedCount.innerText = '0';
+			this.elements.taskCompletedCount.innerText = '0';
+			this.elements.taskList.innerHTML = ' ';
+		}
+	},
 };
 
 application.elements.taskForm.addEventListener('submit', event => {
